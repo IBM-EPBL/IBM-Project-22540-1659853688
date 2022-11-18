e65e1284e47ba8704197c7dad6fbe5747e4cebb9
Shriya <68802716+Shriya0605@users.noreply.github.com>
2022-11-18 21:55:36 +0530

    Update views.py

--- Final Deliverables/Project Source Code/Plasma Donor Application/src/donor/views.py
+++ Final Deliverables/Project Source Code/Plasma Donor Application/src/donor/views.py
@@ -1,13 +1,4 @@
-from django.shortcuts import render,redirect,reverse
-from . import forms,models
-from django.db.models import Sum,Q
-from django.contrib.auth.models import Group
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import login_required,user_passes_test
-from django.conf import settings
-from datetime import date, timedelta
-from django.core.mail import send_mail
-from django.contrib.auth.models import User
+ import User
 from blood import forms as bforms
 from blood import models as bmodels
 
